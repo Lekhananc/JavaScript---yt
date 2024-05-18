530a0369bd1702d0e6a6a2056ba434eee1718ee3
Lekhananc <86131162+Lekhananc@users.noreply.github.com>
2024-05-18 20:23:16 +0000

    learnt more about data types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,43 @@
+"use strict"    //treat all JS code as newer version
+// entire code in the file will be treated as newer code
+// semi colen is not necessory
+// alert("hello")   // we atr using node JS not browser so we cannot use alert
+console.log(3
+    +
+    3) 
+    // no doubt you'll get 6 as an output code should be readable
+// console.log("lekhana")
+
+let name = "Lekhana"    //String
+let age = 18    //number
+let state = null
+let active = " "
+let isLoggedIn = true//boolean
+let user
+
+// premitive
+
+// number range => 2^53
+// bigint
+// String => ""
+//boolean => yes/no
+// null=>standalone value //type=>object
+// undefined=>type undefined
+// symbols => unique
+
+    console.log(name);
+    console.log(age);
+    console.log(state);
+    console.log(active);
+    console.log(isLoggedIn);
+    console.log(user);
+
+    console.log(typeof true);
+    console.log(typeof name);
+    console.log(typeof age);
+    console.log(typeof state);
+    console.log(typeof active);
+    console.log(typeof isLoggedIn);
+    console.log(typeof user);
+
+    // object
