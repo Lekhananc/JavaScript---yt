645c29f0e1739e63f9069a796a94682557d57051
Lekhananc <86131162+Lekhananc@users.noreply.github.com>
2024-05-18 20:42:52 +0000

    type conversion

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = true
+
+// console.log(score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+
+//NaN=>Not A Number
+// "33"=>33
+// "33abc"=>NAN(type number)
+// true=>1, false=0
+// undefined=>undefined
+
+let isloggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isloggedIn);
+// console.log(isloggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof isloggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+// 1=>true 0=>false
+// string value=>true, empty string=>false
+
+let someNum = 123
+let stringNum = String(someNum)
+
+console.log(someNum);
+console.log(stringNum);
+console.log(typeof someNum);
+console.log(typeof stringNum);
