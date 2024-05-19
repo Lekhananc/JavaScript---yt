1e92592664bbc847dfaea1c67210516e05d4692b
Lekhananc <86131162+Lekhananc@users.noreply.github.com>
2024-05-19 06:04:43 +0000

    Done with type conversion

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -28,7 +28,47 @@ let isloggedIn = ""
 let someNum = 123
 let stringNum = String(someNum)
 
-console.log(someNum);
-console.log(stringNum);
-console.log(typeof someNum);
-console.log(typeof stringNum);
+// console.log(someNum);
+// console.log(stringNum);
+// console.log(typeof someNum);
+// console.log(typeof stringNum);
+
+
+// ************************************** Operations *******************************************
+let value = 3;
+let negVal = -value
+// console.log(value);
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(9**2);
+// console.log(2/3)
+// console.log(2%3)
+
+let str1 = "hello"
+let str2 = " lekhana"
+str3 = str1+str2
+// console.log(str3);
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2+2;
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+// preincrement and postincrement study mdn
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment
