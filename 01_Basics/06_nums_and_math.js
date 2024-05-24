6cdf792003bab09fd4c94a625bea63eaed8215cf
Lekhananc <86131162+Lekhananc@users.noreply.github.com>
2024-05-24 12:47:23 +0000

    finished Math lib

--- 01_Basics/06_nums_and_math.js
+++ 01_Basics/06_nums_and_math.js
@@ -20,4 +20,24 @@ console.log((balance.toString()).substring(0,2));
 console.log(balance.toFixed(2));
 
 const otherNumber = 23.45346
-console.log(otherNumber.toPrecision(3));
+console.log(otherNumber.toPrecision(4));
+
+const num2 = 100000000;
+console.log(num2.toLocaleString('en-IN'));
+
+// ----------------------------------Maths-----------------------------------------
+console.log(Math);
+console.log(Math.abs(-45));// gives absolute value
+console.log(Math.round(5.6));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.2));
+console.log(Math.sqrt(9));
+console.log(Math.pow(9,2));
+console.log(Math.min(9,2,5,7,1,8,2));
+console.log(Math.max(9,2,5,7,1,8,2));
+console.log(Math.floor(Math.random()*10)+1);
+// trick 
+const min = 10;
+const max = 20;
+Math.random
+console.log(Math.floor(Math.random())*(max-min+1)+min);
