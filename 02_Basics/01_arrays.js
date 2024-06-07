66298b15f5aa5eb0424e4988dfa6584747c58429
Lekhananc <86131162+Lekhananc@users.noreply.github.com>
2024-06-07 13:49:52 +0000

    Arrays

---
+++ 02_Basics/01_arrays.js
@@ -0,0 +1,27 @@
+const myArr = [0,1,2,3,4,5];
+// arrays can be mixed
+// resizable
+// cannot access using arbitary variable
+// 0 indexed
+// whenever copy operation is performed shallow copy is created
+// console.log(myArr);
+// console.log(myArr[0]);
+
+const breakfast = ['idli','dosa','poha']
+
+const myArr2 = new Array(1,2,3,4,4);
+// console.log(myArr2);
+
+// Array methods
+    // myArr.push(9);
+    // myArr.push(8);
+    // console.log(myArr);
+
+    // myArr.pop();
+    // console.log(myArr);
+    // myArr.unshift(9);   //Adds an element to the start of the array
+    // console.log(myArr);
+
+    myArr.unshift();
+    myArr.shift();
+    console.log(myArr);
