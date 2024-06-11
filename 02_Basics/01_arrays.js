90016f3888c70acaab25ed96954764c7cff8113f
Lekhananc <86131162+Lekhananc@users.noreply.github.com>
2024-06-11 18:58:54 +0000

    Arrays

--- 02_Basics/01_arrays.js
+++ 02_Basics/01_arrays.js
@@ -11,7 +11,7 @@ const breakfast = ['idli','dosa','poha']
 
 const myArr2 = new Array(1,2,3,4,4);
 // console.log(myArr2);
-
+// ----------------------------------------------------
 // Array methods
     // myArr.push(9);
     // myArr.push(8);
@@ -19,9 +19,32 @@ const myArr2 = new Array(1,2,3,4,4);
 
     // myArr.pop();
     // console.log(myArr);
+    // ---------------------------------------------------
     // myArr.unshift(9);   //Adds an element to the start of the array
     // console.log(myArr);
+    // myArr2.unshift(9);
+    // myArr2.shift();
+    // myArr2.shift();
+    // console.log(myArr2);
+    // ---------------------------------------------------
 
-    myArr.unshift();
-    myArr.shift();
+    // console.log(myArr.includes(2));  //true
+    // console.log(myArr.includes(9));  //false
+    // console.log(myArr.indexOf(5));   //5
+    // console.log(myArr.indexOf(9));   //-1(not present)
+    // ----------------------------------------------------
+    // const newArr = myArr.join();
+    // console.log(myArr);
+    // console.log(newArr);    //type is changed to string
+
+    // --------------slice---------------splice------------
+
+    console.log('A',myArr);
+    const myn1 = myArr.slice(1,3); //It does not manupulate the original array and includes stop value
+    console.log(myn1);
+    console.log(myArr);
+
+    console.log('B',myArr);
+    const myn2 = myArr.splice(1,3); //It manupulate the original array and dpes not includes stop value
+    console.log(myn2);
     console.log(myArr);
